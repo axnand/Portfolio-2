8f36f550da128f461d1aaeb2b511b18f5dcb981b
Anand <anandmohanjha241@gmail.com>
2024-07-22 06:32:49 +0530

    insta username changed

--- components/nav/components/MyLinks.tsx
+++ components/nav/components/MyLinks.tsx
@@ -46,7 +46,7 @@ export const MyLinks = () => {
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.3 }}
       >
-        <Link href="https://www.instagram.com/axnand" target="_blank" rel="nofollow">
+        <Link href="https://www.instagram.com/aanaxnd" target="_blank" rel="nofollow">
           <AiOutlineInstagram size="2.4rem" />
         </Link>
       </motion.span>
