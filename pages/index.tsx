4e7ba7b109aeee00d360276465276bd997282d05
Anand <anandmohanjha241@gmail.com>
2024-07-22 14:39:51 +0530

    changed title

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function home() {
   return (
     <>
       <Head>
-        <title>Anand | Building beautiful, functional websites for your business</title>
+        <title>Anand | Portfolio</title>
         <meta name="description" content="I am a web developer experienced in mern stack " />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/Anand.png" />
